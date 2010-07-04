7db160f37d7e6d5442afa6c87d2d28bc49222209
ignatusfordon <ignatusfordon@7fdb7857-aad3-6fc1-1239-45cb07d991c5>
2010-07-04 23:24:11 +0000

    AI implemented but not working yet. Will continue to work on it later.

--- Game Developers Club Puzzle Fighter/Game Developers Club Puzzle Fighter/AI/aiClass.cpp
+++ Game Developers Club Puzzle Fighter/Game Developers Club Puzzle Fighter/AI/aiClass.cpp
@@ -16,38 +16,61 @@ void AI::setNewAI(int DIFFICULTY, int BEHAVIOR)
 	}
 	// TODO: add more behaviors
 }
+int AI::getMove()
+{
+	moveReady = false;
+	return move;
+}
 void AI::update()
 {
-	//if(!behavior->isDone())
-	//{
-	//	behavior->findSpot();
-	//}
-	//else
-	//{
-	//	int now = SDL_GetTicks();
-	//	int timePassed = now-then;
-	//	if(timePassed > delay)
-	//	{
-	//		then = now;
-	//		// TODO: put in algolrithm to move piece to desired position and rotation
-	//		int x = GamePlay::Ins()->getPiece(PLAYER)->getX();
-	//		if(behavior->getLoc().x > x)
-	//		{
-	//			moveReady = true;
-	//			move = eP1Right; //TODO: set these to p2 when second game board is ready
-	//		}
-	//		else if(behavior->getLoc().x < x)
-	//		{
-	//			moveReady = true;
-	//			move = eP1Left; //TODO: set these to p2 when second game board is ready
-	//		}
-	//		else
-	//		{
-	//			moveReady = true;
-	//			move = eP1Left; //TODO: set these to p2 when second game board is ready
-	//		}
-	//	}
-	//}
+	if(!(currentPiece == *GamePlay::Ins()->getBoard(1)->getPiece()))
+	{
+		currentPiece = *GamePlay::Ins()->getBoard(1)->getPiece();
+		piecePlaced();
+	}
+	if(!behavior->isDone())
+	{
+		behavior->findSpot();
+	}
+	else
+	{
+		double now = SDL_GetTicks();
+		double timePassed = now-then;
+		if(timePassed > delay)
+		{
+			then = now;
+			// TODO: put in algolrithm to move piece to desired position and rotation
+			int x = GamePlay::Ins()->getBoard(1)->getPiece()->getX();
+			if(behavior->getLoc()->getD() != currentPiece.getD())
+			{
+				if(currentPiece.getD() - behavior->getLoc()->getD() > (behavior->getLoc()->getD()+4) - (currentPiece.getD()+4))
+				{
+					moveReady = true;
+					move = eInputRotateC;
+				}
+				else
+				{
+					moveReady = true;
+					move = eInputRotateCC;
+				}
+			}
+			if(behavior->getLoc()->getX() > x)
+			{
+				moveReady = true;
+				move |= eInputRight;
+			}
+			else if(behavior->getLoc()->getX() < x)
+			{
+				moveReady = true;
+				move |= eInputLeft;
+			}
+			else
+			{
+				moveReady = true;
+				move |= eInputDown;
+			}
+		}
+	}
 }
 AI::~AI()
 {
--- Game Developers Club Puzzle Fighter/Game Developers Club Puzzle Fighter/AI/behaviorClass.cpp
+++ Game Developers Club Puzzle Fighter/Game Developers Club Puzzle Fighter/AI/behaviorClass.cpp
@@ -1,15 +1,28 @@
 #include "behaviorClass.h"
 
 
+
+behaviorParent::behaviorParent()
+{
+	finished = false;
+	iterator = 0;
+//	desiredPos = new pieceClass();
+}
+behaviorParent::~behaviorParent()
+{
+//	delete [] desiredPos;
+}
 ////////////////////////////////
 // basic behavior definitions //
 ////////////////////////////////
 
 void basicBehavior::findSpot()
 {
-	pos.x = iterator % 6;
-	pos.y = 14;
-	pos.r = 0;
+	//pos.x = iterator % 6;
+	//pos.y = 14;
+	//pos.d = 0;
+	desiredPos.set(iterator % 6, 14, 0);
 	iterator++;
+	finished = true;
 	return;
 }
